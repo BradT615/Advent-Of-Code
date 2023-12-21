d76bd54b5e7ac38cd696ece7990bff4c9dff8d0d
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-12-21 03:49:49 -0500

    Day 4 part 1 complete

---
+++ Day_4-1.py
@@ -0,0 +1,22 @@
+with open("Day_4/input.txt", "r") as file:
+    lines = [line.strip() for line in file]
+
+total = 0
+
+for i in range(0, len(lines)):
+    matches = 0
+    line = lines[i]
+    lead = line.find(": ")
+    line = line[lead + 2:]
+    cards = line.split("|")
+    winningCards = cards[0].split()
+    yourCards = cards[1].split()
+    for card in winningCards:
+        for yourCard in yourCards:
+            if card == yourCard:
+                matches += 1
+    if matches > 0:
+        total += 2 ** (matches - 1)
+        
+print(total)
+file.close()
